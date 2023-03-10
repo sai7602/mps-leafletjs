6238161309c416ef1469dfaacd1753d9370d1c8e
Oleksandr Salivon <sai7602@ukr.net>
2023-03-10 23:04:07 +0200

    added dummy sent server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MapComponent } from './map/map.component';
-import { Map2Component } from './map2/map2.component';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { HttpClientModule } from '@angular/common/http';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
-  declarations: [AppComponent, MapComponent, Map2Component],
-  imports: [BrowserModule, AppRoutingModule, LeafletModule],
+  declarations: [AppComponent, MapComponent],
+  imports: [BrowserModule, AppRoutingModule, LeafletModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,15 +1,8 @@
-import { OnInit, Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
-import { Observable, Subscriber } from 'rxjs';
-import { environment } from 'src/environments/environment';
-import { map } from 'leaflet';
-
-interface CustomPolylineOptions extends L.PolylineOptions {
-  arrowheads?: {
-    size: string;
-    frequency: string;
-  };
-}
+import { parse, stringify, toJSON, fromJSON } from 'flatted';
+import 'leaflet-arrowheads';
+import { MarkerDataService } from '../marker-data-service.service';
 
 @Component({
   selector: 'app-map',
@@ -20,17 +13,22 @@ export class MapComponent implements OnInit {
   private map!: L.Map;
   private markers: L.Marker[] = [];
   private polyline!: L.Polyline;
+  private markerIndex = 0;
 
-  constructor() {}
+  constructor(private markerDataService: MarkerDataService) {}
 
   ngOnInit(): void {
     this.initMap();
   }
 
+  svgMarker = (index: number) => {
+    return `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" class="svg-icon-svg" style="width:52px; height:84"><filter id="iconShadowBlur"><feGaussianBlur in="SourceGraphic" stdDeviation="1"></feGaussianBlur></filter><path filter="url(#iconShadowBlur" )="" class="svg-icon-shadow" d="M 1 16 L 16 46 L 31 16 A 8 8 0 0 0 1 16 Z" fill="rgb(0,0,10)" stroke-width="2" stroke="rgb(0,0,10)" style="opacity: 0.5; transform-origin: 16px 48px; transform: rotate(45deg) translate(0px, 0px) scale(1, 0.75)"></path><path class="svg-icon-path" d="M 1 16 L 16 46 L 31 16 A 8 8 0 0 0 1 16 Z" stroke-width="2" stroke="rgb(0,102,255)" stroke-opacity="1" fill="rgb(0,102,255)" fill-opacity="0.4"></path><circle class="svg-icon-circle" cx="16" cy="16" r="8" fill="rgb(255,255,255)" fill-opacity="1" stroke="rgb(0,102,255)" stroke-opacity="1&quot;" stroke-width="2"></circle><text text-anchor="middle" x="16" y="18.8" style="font-size: 8px; font-weight: normal" fill="rgba(0, 0, 0,1)">${index}</text></svg>`;
+  };
+
   private initMap(): void {
     // create a new map instance
     this.map = L.map('map', {
-      center: [51.505, -0.09],
+      center: [50.4851493, 30.4721233],
       zoom: 13,
     });
 
@@ -52,16 +50,57 @@ export class MapComponent implements OnInit {
   private addMarker(latlng: L.LatLng): void {
     // create a new marker and add it to the map
     const marker = L.marker(latlng, {
-      alt: 'asdas',
       autoPan: true,
       draggable: true,
       riseOnHover: true,
     });
-    marker.addTo(this.map);
+
+    const form = document.createElement('form');
+    form.innerHTML = `
+      <label for="lat">Latitude:</label>
+      <input type="text" name="lat" value="${latlng.lat}" required>
+      <br>
+      <label for="lng">Longitude:</label>
+      <input type="text" name="lng" value="${latlng.lng}" required>
+      <br>
+      <label for="height">Height:</label>
+      <input type="text" name="height" value="0">
+      <br>
+      <button type="submit">Save</button>
+    `;
+
+    marker.bindPopup(form);
 
     // add click event listener to the marker
     marker.on('click', () => {
+      marker.openPopup();
+    });
+
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      // get the updated latitude, longitude, and height values
+      const latInput = form.elements.namedItem('lat') as HTMLInputElement;
+      const lngInput = form.elements.namedItem('lng') as HTMLInputElement;
+      const heightInput = form.elements.namedItem('height') as HTMLInputElement;
+      const lat = parseFloat(latInput.value);
+      const lng = parseFloat(lngInput.value);
+      const height = parseFloat(heightInput.value);
+
+      // update the marker position and height
+      marker.setLatLng([lat, lng]);
+      marker.setZIndexOffset(height);
+
+      // close the popup
+      marker.closePopup();
+
+      // update the polyline
+      this.updatePolyline();
+    });
+
+    marker.on('dblclick', () => {
       this.removeMarker(marker);
+      this.markerIndex--;
     });
 
     // add dragend event listener to the marker
@@ -71,7 +110,6 @@ export class MapComponent implements OnInit {
 
     // add the marker to the markers array
     this.markers.push(marker);
-    console.log(this.markers);
 
     // update the polyline
     this.updatePolyline();
@@ -80,10 +118,14 @@ export class MapComponent implements OnInit {
 
     // create a new marker icon with the marker index
     const icon = L.divIcon({
-      className: 'marker-label',
-      html: `<span>${this.markerIndex}</span>`,
+      className: 'number-icon',
+      iconSize: [25, 41],
+      iconAnchor: [10, 44],
+      popupAnchor: [3, -40],
+      html: this.svgMarker(this.markerIndex),
     });
     marker.setIcon(icon);
+    marker.addTo(this.map);
   }
 
   private removeMarker(marker: L.Marker): void {
@@ -107,16 +149,23 @@ export class MapComponent implements OnInit {
     }
 
     // create a new polyline from the markers
-    const latlngs = this.markers.map((marker) => marker.getLatLng());
-    const options: CustomPolylineOptions = {
-      arrowheads: {
-        size: '10px',
-        frequency: '1',
-      },
-    };
+    const latlngs = this.markers.map((marker, index) => {
+      const icon = L.divIcon({
+        className: 'number-icon',
+        iconSize: [25, 41],
+        iconAnchor: [10, 44],
+        popupAnchor: [3, -40],
+        html: this.svgMarker(index + 1),
+      });
+      return marker.getLatLng();
+    });
 
     if (latlngs.length > 1) {
-      this.polyline = L.polyline(latlngs, options);
+      this.polyline = L.polyline(latlngs, { color: 'red' }).arrowheads({
+        color: 'red',
+        yawn: 20,
+        fill: true,
+      });
       this.polyline.addTo(this.map);
     }
   }
@@ -134,6 +183,21 @@ export class MapComponent implements OnInit {
       this.polyline.removeFrom(this.map);
       // this.polyline = null;
     }
+    this.markerIndex = 0;
   }
-  private markerIndex = 0;
+
+  sendMarkerData() {
+    // send marker data to server using the MarkerDataService
+
+    this.markerDataService
+      .sendMarkerDataToServer(stringify(this.markers))
+      .subscribe({
+        next: (response) => {
+          console.log('Marker data sent successfully', response);
+        },
+        error: (error) => {
+          console.log('Error sending marker data', error);
+        },
+      });
+  }
 }
